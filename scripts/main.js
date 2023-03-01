f7159d5715d422e277949100c448ab8a0ba9398b
joanbaldo <joanbaldo@hotmail.com>
2023-03-01 21:30:58 +0100

    EX JSON 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
-console.log('CONNECT');
+// console.log('CONNECT');
+// localStorage.setItem('user','grover');
+// localStorage.setItem('user','joan');
+// localStorage.setItem('master','alfonsin');
+
+/// EJERCICIOS
+//1 - Crear un formulario de contacto con los siguientes campos:
+// Nombre 
+// Correo 
+// Mensaje 
+
+//2 - Guardar en Local Storage los datos de contacto rellenados 
+// del usuario (solo guarda un usuario)
+const formNombre = document.getElementById('nombre')
+console.log(formNombre)
+const formButtonEnviar = document.getElementById('btsend')
+
+function onSubmit(e) {
+    e.preventDefault()
+    console.log('hola')
+}
+
